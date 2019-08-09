650f7432e5f53ad639e0b787a86ca90d3d8b6f8d
yashu <yashu.tanna@outlook.com>
2019-08-09 20:50:08 +0200

    change photo and change info

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.how2happy.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'How 2 Happy',
+  subtitle: 'How2Happy aims at discovering way to find the happy in our everyday lives.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: '',
   useKatex: false,
   menu: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Mishka Karsan',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Mishka is an intern practicing medicine in Cape Town, South Africa.',
     contacts: {
       email: '#',
       telegram: '#',
